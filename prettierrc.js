934cfacdf636d4ad673e5cf44efdcbf5a2bbc9ff
Andrew <hajerbin@foxmail.com>
2021-07-07 10:48:20 +0800

    chore: add prettierrc file

---
+++ prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // https://github.com/npetruzzelli/prettier-config-standard/blob/06e50ed/README.markdown#usage
+    ...require('prettier-config-standard'),
+  
+    // See https://github.com/gtvorg/gtv-webapp-poc/pull/35#issuecomment-671937030
+    bracketSpacing: false,
+    trailingComma: 'all',
+  }
+  
